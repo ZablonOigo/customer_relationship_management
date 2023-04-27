c960db783c51378a35ca7aac64a554fca9849c68
zablon <imjunior254@gmail.com>
2023-04-27 12:37:17 +0300

    datasets

--- web/models.py
+++ web/models.py
@@ -1,14 +1,21 @@
 from django.db import models
 
+class Department(models.Model):
+    title=models.CharField(max_length=50)
+    def __str__(self):
+      return self.title
+    class Meta:
+        ordering=['-title']
 class Record(models.Model):
-    first_name=models.CharField(max_length=50)
-    last_name=models.CharField(max_length=50)
+    first_name=models.CharField(max_length=50, null=True, blank=True)
+    last_name=models.CharField(max_length=50, null=True, blank=True)
+    departments=models.ForeignKey(Department, on_delete=models.SET_NULL, null=True)
     address=models.CharField(max_length=50)
-    email=models.CharField(max_length=50)
-    phone=models.CharField(max_length=50)
-    city=models.CharField(max_length=50)
-    state=models.CharField(max_length=50)
-    zipcode=models.CharField(max_length=50)
+    email=models.CharField(max_length=50, null=True, blank=True)
+    phone=models.CharField(max_length=50, null=True, blank=True)
+    city=models.CharField(max_length=50, null=True, blank=True)
+    state=models.CharField(max_length=50 , null=True, blank=True)
+    zipcode=models.CharField(max_length=50 , null=True, blank=True)
     created_at=models.DateTimeField(auto_now_add=True)
     class Meta:
         verbose_name_plural='Records'
