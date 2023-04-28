668f3db0933f1e48699a2bf1ab73379894d273c2
zablon <imjunior254@gmail.com>
2023-04-28 14:36:30 +0300

    styling create form

--- web/views.py
+++ web/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 from django.db.models import Q
 from .forms import *
+from django.contrib import messages
 def index(request):
     records=Record.objects.all()
     departments=Department.objects.all()
@@ -34,3 +35,11 @@ def create(request):
     if request.method =="GET":
         form=RecordForm()
         return render(request, 'web/create.html',{'form':form} )
+    elif request.method == 'POST':
+        form=RecordForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "New record has been added successfully")
+            return redirect('web:index')
+        else:
+            return render(request, 'web/create.html', {'form':form})
